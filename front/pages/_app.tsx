01c094d37d9ebf22589c1f9777c7940aae37f945
domrut <d.rutkauskas@protonmail.com>
2023-08-17 19:21:56 +0300

    header/introduction

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,5 +1,10 @@
 import "../styles/globals.css"
 import {AppProps} from "next/app";
+import Layout from "../components/layout";
 export default function App({Component, pageProps}: AppProps) {
-    return <Component {...pageProps}/>;
+    return (
+        <Layout>
+            <Component {...pageProps}/>
+        </Layout>
+    )
 }
---
+++ front/pages/posts/[...params].tsx
@@ -0,0 +1,11 @@
+export default function Params () {
+    return (
+        <nav>
+            <ul>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+        </nav>
+    )
+}
