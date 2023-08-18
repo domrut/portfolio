8d090661f291df4bb634cfe90ef45dfa82818512
domrut <d.rutkauskas@protonmail.com>
2023-08-18 16:12:55 +0300

    database connection

--- front/components/experiences.tsx
+++
@@ -1,7 +0,0 @@
-export default function Experiences ({section}) {
-    return (
-        <>
-            <p>{section}</p>
-        </>
-    )
-}
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
+import {MongoClient} from "mongodb";
 import Header from "../components/header";
 import Introduction from "../components/introduction";
 import Nav from "../components/nav";
-import {useState} from "react";
-import Experiences from "../components/experiences";
+import {Key, useState} from "react";
+import Experience from "../components/experience";
+import {GetStaticProps} from "next";
 
-export default function Home() {
+export default function Home({experiences}) {
     const [section, setSection] = useState<string>("Experience")
     return (
         <>
@@ -17,8 +19,30 @@ export default function Home() {
             <main>
                 <Introduction/>
                 <Nav section={section} setSection={setSection}/>
-                <Experiences section={section}/>
+                {experiences.map((el: { _id: Key; company: string; job: string; end_date: string; start_end: string; responsibilities: string[]; }) => {
+                    return (
+                        <Experience
+                            key={el._id}
+                            company={el.company}
+                            job={el.job}
+                            endDate={el.end_date}
+                            startDate={el.start_end}
+                            responsibilities={el.responsibilities}
+                        />
+                    )
+                })}
             </main>
         </>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const client = await MongoClient.connect(process.env.DBKEY);
+    const myData1 = await client.db().collection("experience").find().toArray();
+    await client.close();
+    return {
+        props: {
+            experiences: JSON.parse(JSON.stringify(myData1))
+        }
+    }
 }
