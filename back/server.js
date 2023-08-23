8d778ef596f49253469203a34da7485faaed256d
domrut <d.rutkauskas@protonmail.com>
2023-08-23 19:52:59 +0300

    final

--- back/server.js
+++
@@ -1,7 +0,0 @@
-function test () {
-    fetch("https://www.wix.com/_serverless/hiring-task-spreadsheet-evaluator/sheets")
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-}
-test();
